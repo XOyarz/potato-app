18dd7ae1669b89dfc749b9d2eb4c80b9e484bcf1
Xavier <xavier982@hotmail.com>
2017-12-19 17:12:44 +0100

    Fixed Bug 1: Forms not rendering

--- tracker/settings.py
+++ tracker/settings.py
@@ -28,11 +28,13 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+
 INTERNAL_IPS = (
     '127.0.0.1',
     'localhost',
     '::1'
 )
+ALLOWED_HOSTS = (INTERNAL_IPS)
 
 # Application definition
 
@@ -137,7 +139,9 @@ if DEBUG:
     CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
     CSP_IMG_SRC += ("*.placehold.it",)
 
-CRISPY_TEMPLATE_PACK = 'foundation-5'
+#CRISPY_TEMPLATE_PACK = 'foundation-5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = ('bootstrap', 'uni_form', 'bootstrap3', 'bootstrap4', 'foundation-5')
+
 
 AUTH_USER_MODEL = 'djangae.GaeUser'
 
