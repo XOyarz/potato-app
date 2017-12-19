f8f001589555917c54782cc43fd780a51b9ff3f5
Xavier <xavier982@hotmail.com>
2017-12-19 18:01:12 +0100

    Added Ticket Delete option

--- tracker/site/urls.py
+++ tracker/site/urls.py
@@ -6,6 +6,7 @@ from .views import (
 	update_project_view,
 	project_view,
 	create_ticket_view,
+	delete_ticket_view,
 	update_ticket_view,
 	project_list_view,
 	update_project_view
@@ -26,6 +27,12 @@ urlpatterns = patterns(
     	update_ticket_view,
     	name='ticket-update'
 	),
+	 url(
+			r'^projects/(?P<project_id>\d+)/tickets/(?P<ticket_id>\d+)/delete$',
+			delete_ticket_view,
+			name='ticket-delete'
+		),
+
     url(
     	r'^projects/(?P<project_id>\d+)/edit/$',
     	update_project_view,
